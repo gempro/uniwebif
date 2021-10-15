3afb2a0a7b9ab2968f006a522c7e620d1388ebb0
gempro <geminprojec@gmx.net>
2021-10-15 19:50:21 +0200

    Update 2.0

--- functions/search_list_inc.php
+++ functions/search_list_inc.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <script src="js/jquery-ui.min.js" type="text/javascript"></script>
-<script src="js/tag-it.min.js" type="text/javascript"></script>
+<script src="js/tag-it.js" type="text/javascript"></script>
 <script>
 $(function(){
 	$('#tag_list*').tagit();
@@ -21,9 +21,9 @@ $(function(){
 //
 	include("../inc/dashboard_config.php");
 	
-	if(!isset($_REQUEST['delete_id']) or $_REQUEST['delete_id'] == ''){ $_REQUEST['delete_id'] = '';
+	if(!isset($_REQUEST['delete_id']) or $_REQUEST['delete_id'] == ''){ $_REQUEST['delete_id'] = ''; 
 	
-	} else {
+	} else { 
 	
 	$delete_id = $_REQUEST['delete_id'];
 	
@@ -35,11 +35,9 @@ $(function(){
 	}
 	
 	// sort saved search
-	if(!isset($_REQUEST['sort_list']) or $_REQUEST['sort_list'] == ''){ 
+	if(!isset($_REQUEST['sort_list']) or $_REQUEST['sort_list'] == ''){ $sortby = $search_list_sort; 
 	
-	$sortby = $search_list_sort; 
-	
-	} else {
+	} else { 
 	
 	$sort_list = $_REQUEST['sort_list'];
 	
@@ -58,13 +56,13 @@ $(function(){
 	$group_title = '';
 	
 	// record location
-	$sql_1 = "SELECT * FROM `record_locations` ORDER BY `id` ASC";
+	$sql_1 = "SELECT * FROM `record_locations` WHERE `device` = '0' ORDER BY `id` ASC";
 	if ($result_1 = mysqli_query($dbmysqli,$sql_1))
 	{
 	while ($obj = mysqli_fetch_object($result_1)) {	
 	{
 	if(!isset($record_dropdown) or $record_dropdown == ''){ $record_dropdown = ''; }
-	$record_dropdown = $record_dropdown.'<option value="'.$obj->e2location.'">'.$obj->e2location.'</option>'; 
+	$record_dropdown .= '<option value="'.$obj->id.'">'.$obj->e2location.'</option>';
 	}
 	}
 	}
@@ -77,12 +75,33 @@ $(function(){
 	$servicename_enc = rawurldecode($obj->servicename_enc);
 	{
 	if(!isset($channel_dropdown) or $channel_dropdown == ''){ $channel_dropdown = ''; }
-	$channel_dropdown = $channel_dropdown.'<option value="'.$obj->e2servicereference.'">'.$servicename_enc.'</option>'; 
+	$channel_dropdown .= '<option value="'.$obj->e2servicereference.'">'.$servicename_enc.'</option>';
 	}
 	}
 	}
 	
+	// device dropdown, color, description
+	$sql_3 = "SELECT * FROM `device_list` ORDER BY `id` ASC";
+	if ($result_3 = mysqli_query($dbmysqli,$sql_3))
+	{
+	$device_desc[0] = 'default';
+	$device_color[0] = '#ddd';
+	$device_dropdown = '';
+	while ($obj = mysqli_fetch_object($result_3)){
+	{
+	$id = $obj->id;
+	$device_description = rawurldecode($obj->device_description);
+	$device_desc[$obj->id] = $device_description;
+	$device_color[$obj->id] = $obj->device_color;
+	
+	if(!isset($device_dropdown) or $device_dropdown == ''){ $device_dropdown = ''; }
+	$device_dropdown .= '<option value="'.$id.'" >'.$device_description.'</option>';
+	}
+	}
+	}
+
 	if(!isset($sort_list) or $sort_list == ''){ $sort_list = ''; }
+	
 	if($sort_list == 'searchterm' or $search_list_sort == 'searchterm')
 	{
 	$scroll_buttons = '
@@ -120,12 +139,12 @@ $(function(){
 	} else { $scroll_buttons = ''; }
 	
 	// saved search list
-	$sql_3 = "SELECT * FROM `saved_search` ORDER BY ".$sortby." ".$sortby1." ";
-	if ($result_3 = mysqli_query($dbmysqli,$sql_3))
+	$sql_4 = "SELECT * FROM `saved_search` ORDER BY ".$sortby." ".$sortby1." ";
+	if ($result_4 = mysqli_query($dbmysqli,$sql_4))
 	{
-	while ($obj = mysqli_fetch_object($result_3)) {
+	while ($obj = mysqli_fetch_object($result_4)) {
 	
-	if(!isset($saved_search_list) or $saved_search_list == ""){ $saved_search_list = ""; }
+	if(!isset($saved_search_list) or $saved_search_list == ''){ $saved_search_list = ''; }
 	
 	$servicename_enc = rawurldecode($obj->servicename_enc);
 	$searchterm = rawurldecode($obj->searchterm);
@@ -137,6 +156,7 @@ $(function(){
 	$record_location = $obj->e2location;
 	$activ = $obj->activ;
 	$rec_replay = $obj->rec_replay;
+	$device = $obj->device;
 	
 	if(!isset($exclude_channel) or $exclude_channel == ''){ $exclude_channel = ''; }
 	if(!isset($exclude_title) or $exclude_title == ''){ $exclude_title = ''; }
@@ -149,8 +169,8 @@ $(function(){
 	if($search_option == 'title'){ $selected7 = 'selected'; } else { $selected7 = ''; }
 	if($search_option == 'description'){ $selected8 = 'selected'; } else { $selected8 = ''; }
 	if($search_option == 'extdescription'){ $selected9 = 'selected'; } else { $selected9 = ''; }
-	if($activ == 'yes'){ $selected10 = 'selected'; } else { $selected10 = ''; }
-	if($activ == 'no'){ $selected11 = 'selected'; } else { $selected11 = ''; }
+	if($activ == 'yes'){ $active_checkbox = 'checked'; } else { $active_checkbox = ''; }
+	//if($activ == 'no'){ $selected11 = 'selected'; } else { $selected11 = ''; }
 	if($activ == 'yes'){ $as = 'success'; } else { $as = 'danger'; }
 	if($obj->e2eventservicename == 'NULL'){ $servicename_enc = 'All Channels'; }
 		
@@ -178,13 +198,12 @@ $(function(){
 	if($search_option == 'all'){ $search_option_desc = 'All'; }
 	if($search_option == 'title'){ $search_option_desc = 'Title'; }
 	if($search_option == 'description'){ $search_option_desc = 'Description'; }
-	if($search_option == 'extdescription'){ $search_option_desc = 'Extended description'; }
+	if($search_option == 'extdescription'){ $search_option_desc = 'Ext. description'; }
 	
 	if($time_format == '1')
 	{
 	$last_change = date('d.m.Y - H:i', $obj->last_change);
 	$last_crawl = date('d.m.Y - H:i', $obj->last_crawl);
-	//
 	if($obj->last_change == '0'){ $last_change = 'not changed'; }
 	if($obj->last_crawl == '0'){ $last_crawl = 'not crawled'; }
 	}
@@ -193,28 +212,46 @@ $(function(){
 	{
 	$last_change = date('n/d/Y - g:i A', $obj->last_change);
 	$last_crawl = date('n/d/Y - g:i A', $obj->last_crawl);
-	//
 	if($obj->last_change == '0'){ $last_change = 'not changed'; }
 	if($obj->last_change == '0'){ $last_crawl = 'not crawled'; }
 	}
 	
-	//
 	if($sortby == 'searchterm')
 	{
-	if($group == strtoupper($searchterm[0][0])){ $group_title = ''; } else { $group_title = strtoupper($obj->searchterm[0][0]); }
+	if($group == strtoupper($searchterm[0][0])){ $group_title = ''; 
+	} else { 
+	$group_title = strtoupper($obj->searchterm[0][0]);
+	if($group_title == '%'){ $group_title = '&nbsp;'; }
+	}
 	$group = strtoupper($obj->searchterm[0][0]);
 	}
 	
-	// get record location id
-	$sql_4 = mysqli_query($dbmysqli, "SELECT * FROM `record_locations` WHERE `e2location` = '".$record_location."' LIMIT 0,1");
-	$result_4 = mysqli_fetch_assoc($sql_4);
-	$rec_location_id = $result_4['id'];
+	// record location id
+	$sql_5 = mysqli_query($dbmysqli, "SELECT `id` FROM `record_locations` WHERE `device` = '".$obj->device."' AND `e2location` = '".$record_location."' LIMIT 0,1");
+	$result_5 = mysqli_fetch_assoc($sql_5);
+	$rec_location_id = $result_5['id'];
+	
+	// record location different device
+	if($obj->device != 0)
+	{
+	$record_dropdown = '';
+	$sql_6 = "SELECT * FROM `record_locations` WHERE `device` = '".$obj->device."' ORDER BY `id` ASC";
+	if ($result_6 = mysqli_query($dbmysqli,$sql_6))
+	{
+	while ($obj_1 = mysqli_fetch_object($result_6)) {	
+	{
+	if(!isset($record_dropdown) or $record_dropdown == ''){ $record_dropdown = ''; }
+	$record_dropdown .= '<option value="'.$obj_1->id.'">'.$obj_1->e2location.'</option>';
+	}
+	}
+	}
+	}
 
 	$saved_search_list = $saved_search_list."
 	<div id=\"list_$group_title\">
-	<div id=\"search_list_div_$obj->id\">
 	<strong>$group_title</strong>
-	<div id=\"saved_search_list\">
+	<div id=\"search_list_div_$obj->id\">
+	<div id=\"saved_search_list\" style=\"border: 1px solid $device_color[$device] !important;\">
 	  <div class=\"row\">
 		<div id=\"saved_search_list_$obj->id\" onclick=\"saved_search_list_edit(this.id);\" style=\"cursor: pointer;\">
 		  <div class=\"col-md-3\">
@@ -229,10 +266,10 @@ $(function(){
 			<p>Channel<br>
 			  $servicename_enc</p>
 		  </div>
-		  <div class=\"col-md-2\">
+		  <div class=\"col-md-3\">
 			<p>$obj->e2location</p>
 		  </div>
-		  <div class=\"col-md-2\">
+		  <div class=\"col-md-1\">
 			<p>activ<br>
 			<span class=\"badge-$as\">$obj->activ</span></p>
 		  </div>
@@ -246,10 +283,10 @@ $(function(){
 		  </div>	  
 		  <div class=\"col-md-2\">
 			<p><select id=\"searcharea_$obj->id\" class=\"search_list_area\">
-			<option value=\"all\" $selected6>all</option>
-			<option value=\"title\" $selected7>title</option>
-			<option value=\"description\" $selected8>description</option>
-			<option value=\"extdescription\" $selected9>ext. description</option>
+			<option value=\"all\" $selected6>All</option>
+			<option value=\"title\" $selected7>Title</option>
+			<option value=\"description\" $selected8>Description</option>
+			<option value=\"extdescription\" $selected9>Ext. description</option>
 		  </select></p>
 		  </div>
 		  <!---->
@@ -260,24 +297,31 @@ $(function(){
 			$channel_dropdown</select></p>
 		  </div>
 		  <!---->
-		  <div class=\"col-md-2\">
+		  <div class=\"col-md-3\">
 			<p><select id=\"rec_dropdown_saved_search_list_$obj->id\" value=\"$obj->e2location\" class=\"search_list_rec_location\">
 			<option value=\"\"></option>
 			$record_dropdown
 			</select></p>
 		  </div>
-		  <div class=\"col-md-2\">
-			<p><select id=\"active_$obj->id\" class=\"search_list_active\">
-			<option value=\"yes\" $selected10>yes</option>
-			<option value=\"no\" $selected11>no</option>
-		  </select></p>
+		  <div class=\"col-md-1\">
+		  <p class=\"saved_search_chkbox\"><input id=\"active_$obj->id\" type=\"checkbox\" id=\"checkbox\" $active_checkbox>
+		  <span class=\"chkbox_info\">Activate / Deactivate</span>
+		  </p>
 		  </div>
 		  </div>
 		  <div class=\"row\">
 		  <div class=\"col-md-3\">
-			<p>Timer for repeating Broadcast's<br><select id=\"rec_replay_$obj->id\" class=\"search_list_replay\">
+			<p>Timer for repeating broadcast's<br><select id=\"rec_replay_$obj->id\" class=\"search_list_replay\">
 			<option value=\"yes\" $selected4>yes</option>
 			<option value=\"no\" $selected5>no</option>
+		  </select></p>
+		  </div>
+		  <div class=\"col-md-2\">
+			<p>Record device<br>
+			<select id=\"rec_device_$obj->id\" class=\"device_list\" onChange=\"saved_search_change_device(this.id)\">
+			<option selected disabled>$device_desc[$device]</option>
+			<option value=\"0\">default</option>
+			$device_dropdown
 		  </select></p>
 		  </div> 
 		  </div><!-- row -->
@@ -314,10 +358,10 @@ $(function(){
 		  </div><!-- row-->
 		  <div class=\"row\">
 		  <div class=\"col-md-12\">
-		  <a id=\"search_link_$obj->id\" href=\"search.php?searchterm=$obj->searchterm&option=$search_option&record_location=$rec_location_id&exclude_channel=$obj->exclude_channel&exclude_title=$obj->exclude_title&exclude_description=$obj->exclude_description&exclude_extdescription=$obj->exclude_extdescription&search_channel=$search_channel&channel_id=$channel_id&rec_replay=$rec_replay&search_id=$obj->id\" target=\"_blank\" title=\"Show results\">
+		  <a id=\"search_link_$obj->id\" href=\"search.php?searchterm=$obj->searchterm&option=$search_option&record_location=$rec_location_id&exclude_channel=$obj->exclude_channel&exclude_title=$obj->exclude_title&exclude_description=$obj->exclude_description&exclude_extdescription=$obj->exclude_extdescription&search_channel=$search_channel&channel_id=$channel_id&device=$obj->device&rec_replay=$rec_replay&search_id=$obj->id\" target=\"_blank\" title=\"Show results\">
 		  <i class=\"fa fa-search fa-1x\"></i></a>
-		<input id=\"saved_search_list_save_btn_$obj->id\" type=\"submit\" onClick=\"saved_search_list_save(this.id)\" value=\"SAVE\" class=\"btn btn-success btn-sm\">
-		<input id=\"saved_search_list_delete_btn_$obj->id\" type=\"submit\" onClick=\"saved_search_list_delete(this.id)\" value=\"DELETE\" class=\"btn btn-danger btn-sm\"/>
+		<input id=\"saved_search_list_save_btn_$obj->id\" type=\"submit\" onClick=\"saved_search_list_save(this.id)\" value=\"SAVE\" class=\"btn btn-success btn-xs\">
+		<input id=\"saved_search_list_delete_btn_$obj->id\" type=\"submit\" onClick=\"saved_search_list_delete(this.id)\" value=\"DELETE\" class=\"btn btn-danger btn-xs\"/>
 		<span id=\"saved_search_list_scroll_timer_$obj->id\"></span>
 		<span id=\"saved_search_list_status_$obj->id\"></span>
 		<div class=\"crawl-info\"><span aria-hidden=\"true\">Last change: <span id=\"last_change_$obj->id\">$last_change</span> | Last crawl: $last_crawl</span></div>
@@ -333,7 +377,9 @@ $(function(){
 	";
 	}
 	}
-	if(!isset($saved_search_list) or $saved_search_list == ""){ $saved_search_list = "No saved searches..<hr />"; } else { echo $scroll_buttons.$saved_search_list; }
+	if(!isset($saved_search_list) or $saved_search_list == ''){ $saved_search_list = 'No saved searches..<hr />'; } else { echo $scroll_buttons.$saved_search_list; }
+
 ?>
+
 </body>
 </html>
